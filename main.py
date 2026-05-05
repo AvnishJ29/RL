81c377e84cf127be974ea3d8d413818aba51825b
AvnishJ29 <151904853+AvnishJ29@users.noreply.github.com>
2026-05-05 10:07:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ from plot           import load_seed_logs, aggregate_logs, plot_curves, plot_hov
 # Global constants
 # ─────────────────────────────────────────────────────────────────────────────
 
-SEEDS         = [62, 12, 36, 20, 24, 16, 58]
+SEEDS         = [69, 30, 60]
 N_SEEDS       = 15
 EVAL_FREQ     = 10_000
 EVAL_EPISODES = 20
